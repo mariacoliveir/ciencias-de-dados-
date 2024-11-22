832cba5bc137bd77a010014364337a879b71420f
mariacoliveir <160596961+mariacoliveir@users.noreply.github.com>
2024-11-22 11:55:00 -0300

    .

--- graficos/quatidadedeUsuarios.js
+++ graficos/quatidadedeUsuarios.js
@@ -1,6 +1,6 @@
 
 async function quantidadeUsuarios() {
-    const url = ' '
+    const url = 'https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/trabalho/trabalho-dados-gerais.json '
     const res = await fetch(url)
     const dados = await res.json()
     const pessoas_mundo = Object.keys(dados)
